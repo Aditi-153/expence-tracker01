56948b59deb83f08613099d4fb234a6e389c18a1
Aditi-153 <pachpatiladiti@gmail.com>
2026-09-20 20:56:44 +0530

    changes in routes

--- backend/controller/expense.controller.js
+++ backend/controller/expense.controller.js
@@ -1,6 +1,5 @@
 import Expense from "../models/expense.model.js";
 
-
 export const addExpense = async (req, res) => {
   try {
     const { title, amount, type, category, date } = req.body;
@@ -32,7 +31,6 @@ export const addExpense = async (req, res) => {
   }
 };
 
-
 export const getExpenses = async (req, res) => {
   try {
     const expenses = await Expense.find().sort({
@@ -45,6 +43,81 @@ export const getExpenses = async (req, res) => {
 
     res.status(500).json({
       message: "Failed to fetch transactions",
+    });
+  }
+};
+
+export const getExpense = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const expense = await Expense.findById(id);
+
+    if (!expense) {
+      return res.status(404).json({
+        message: "Expense not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Expense fetched successfully",
+      expense,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to fetch expense",
+      error: error.message,
+    });
+  }
+};
+
+export const updateExpense = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const expense = await Expense.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!expense) {
+      return res.status(404).json({
+        message: "Expense not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Expense updated successfully",
+      expense,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to update expense",
+      error: error.message,
+    });
+  }
+};
+
+export const deleteExpense = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const expense = await Expense.findByIdAndDelete(id);
+
+    if (!expense) {
+      return res.status(404).json({
+        message: "Expense not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Expense deleted successfully",
+      expense,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to delete expense",
+      error: error.message,
     });
   }
 };
--- backend/routes/expense.routes.js
+++ backend/routes/expense.routes.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { addExpense, getExpenses } from "../controller/expense.controller.js";
+import { addExpense, deleteExpense, getExpense, getExpenses, updateExpense } from "../controller/expense.controller.js";
 
 const router = express.Router();
 
 router.post("/", addExpense);
 router.get("/", getExpenses);
+router.get("/:id" , getExpense);
+router.patch("/:id" , updateExpense);
+router.delete("/:id",deleteExpense);
 
 
 export default router;
