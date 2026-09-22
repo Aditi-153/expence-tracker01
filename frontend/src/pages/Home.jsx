667860fe554f2494ae13c8d6fc69cf3cab37739d
Aditi-153 <pachpatiladiti@gmail.com>
2026-09-22 16:55:22 +0530

    added delete and update button

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
-import axios from "axios"
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-
   const navigate = useNavigate();
   const [expenses, setExpenses] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [editExpense, setEditExpense] = useState(null);
 
   useEffect(() => {
     const fetchExpenses = async () => {
@@ -25,6 +25,41 @@ const Home = () => {
 
     fetchExpenses();
   }, []);
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`${import.meta.env.VITE_DELETE_EXPENSE}/${id}`, {
+        withCredentials: true,
+      });
+      setExpenses((preExpenses) =>
+        preExpenses.filter((expense) => expense._id !== id),
+      );
+    } catch (error) {
+      alert("failed to delete !", error);
+    }
+  };
+
+  const handleUpdate = async (id, updatedData) => {
+    try {
+      const response = await axios.patch(
+        `${import.meta.env.VITE.UPDATE_EXPENSE}/${id}`,
+        updatedData,
+        {
+          withCredentials: true,
+        },
+      );
+
+      setExpenses((prevExpenses) =>
+        prevExpenses.map((expense) =>
+          expense._id === id ? response.data.expense : expense,
+        ),
+      );
+
+      setEditExpense(null);
+    } catch (error) {
+      alert("failed to update !", error);
+    }
+  };
 
   return (
     <div className="min-h-screen bg-slate-100 p-6">
@@ -105,6 +140,18 @@ const Home = () => {
                   >
                     ₹{expense.amount}
                   </p>
+                  <button
+                    onClick={() => handleDelete(expense._id)}
+                    className="bg-red-500 text-white px-2 py-1 rounded-lg mt-3"
+                  >
+                    Delete
+                  </button>
+                  <button
+                    onClick={() => handleUpdate(expense._id)}
+                    className="bg-green-500 text-white px-2 py-1 rounded-lg mt-3 ml-3"
+                  >
+                    Update
+                  </button>
                 </div>
               </div>
             ))}
