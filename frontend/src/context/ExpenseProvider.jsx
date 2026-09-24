6ad2fa4595f8055187e1f10b5f7a43f7d2f28b56
Aditi-153 <pachpatiladiti@gmail.com>
2026-09-25 02:14:27 +0530

    fixed

---
+++ frontend/src/context/ExpenseProvider.jsx
@@ -0,0 +1,81 @@
+import { useState, useCallback } from "react";
+import axios from "axios";
+import { ExpenseContext } from "./ExpenseContext";
+
+export const ExpenseProvider = ({ children }) => {
+  const [expenses, setExpenses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchExpenses = useCallback(async () => {
+    try {
+      setLoading(true);
+
+      const response = await axios.get(
+        import.meta.env.VITE_EXPENSE_DATA,
+        {
+          withCredentials: true,
+        }
+      );
+
+      setExpenses(response.data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(
+        `${import.meta.env.VITE_EXPENSE_DATA}/${id}`
+      );
+
+      setExpenses((prevExpenses) =>
+        prevExpenses.filter(
+          (expense) => expense._id !== id
+        )
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleUpdate = async (id, updatedData) => {
+    try {
+      const response = await axios.patch(
+        `${import.meta.env.VITE_EXPENSE_DATA}/${id}`,
+        updatedData,
+        {
+          withCredentials: true,
+        }
+      );
+
+      setExpenses((prevExpenses) =>
+        prevExpenses.map((expense) =>
+          expense._id === id
+            ? response.data.expense
+            : expense
+        )
+      );
+    } catch (error) {
+      console.log(error);
+      alert("Failed to update!");
+    }
+  };
+
+  return (
+    <ExpenseContext.Provider
+      value={{
+        expenses,
+        setExpenses,
+        handleDelete,
+        handleUpdate,
+        fetchExpenses,
+        loading,
+      }}
+    >
+      {children}
+    </ExpenseContext.Provider>
+  );
+};
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
 
   useEffect(() => {
     fetchExpenses();
-  }, [fetchExpenses]);
+  }, []);
 
   const totalIncome = useMemo(() => {
     return expenses
